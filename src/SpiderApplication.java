d2fea8916ccab914e9050955303e283a0e7ed126
ZOU <hzouaf@ust.hk>
2026-04-22 18:41:23 +0800

    [fix] refactor project structure

--- src/SpiderApplication.java
+++ src/SpiderApplication.java
@@ -18,6 +18,14 @@ public final class SpiderApplication {
             return;
         }
 
+        // Input example:
+        /**
+         * java -cp out hk.ust.csit5930.spider.SpiderApplication 
+         * https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm   <- startUrl
+         * 300                                                      <- maxPages 
+         * data/spider                                              <- storageDir (optional, default: data/spider)
+         * true                                                      <- sameHostOnly (optional, default: true)
+         */
         String startUrl = args[0];
         int maxPages = Integer.parseInt(args[1]);
         Path storageDir = args.length >= 3
@@ -25,18 +33,21 @@ public final class SpiderApplication {
             : Paths.get("data", "spider").toAbsolutePath().normalize();
         boolean sameHostOnly = args.length < 4 || Boolean.parseBoolean(args[3]);
 
+        // Create a record for the spider configuration, which is an immutable data holder
         SpiderConfig config = new SpiderConfig(
             startUrl,
             maxPages,
             storageDir,
             sameHostOnly,
             15,
-            "CSIT5930Spider/1.0 (+https://github.com/)"
+            "CSIT5930Spider/1.0 (+https://github.com/)"             // Default ID of the crawler, can be customized
         );
 
+        // Create the repository and index consumer, then start the spider
         SpiderRepository repository = new SpiderRepository(storageDir);
         FilePageIndexConsumer indexConsumer = new FilePageIndexConsumer(storageDir.resolve("indexer_input"));
         Spider spider = new Spider(config, repository, indexConsumer);
+        // Start to crawl with the given configuration, repository and index consumer
         spider.crawl();
     }
 
--- src/core/SpiderConfig.java
+++ src/core/SpiderConfig.java
@@ -2,6 +2,7 @@ package hk.ust.csit5930.spider.core;
 
 import java.nio.file.Path;
 
+// record class from Java 14+, immutable data holder for spider configuration
 public record SpiderConfig(
     String startUrl,
     int maxPages,
